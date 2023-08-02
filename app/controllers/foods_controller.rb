8910addc83a09ebe8627b8098eb4e49271418566
Booze33 <tebetisloh@gmail.com>
2023-08-02 04:07:47 -0500

    Added food list

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,61 @@
+# class FoodsController < ApplicationController
+#   def index
+#     @user = current_user
+#     @foods = @user&.foods || []
+#   end
+
+#   def show
+#     @food = Food.find(params[:id])
+#   end
+
+#   def new
+#     @food = Food.new
+#   end
+
+#   def create
+#     @food = Food.new(food_params.merge(user: current_user))
+  
+#     if @food.save
+#       flash[:success] = 'Food added successfully'
+#       redirect_to root_path
+#     else
+#       flash.now[:error] = 'Failed to add food'
+#       render :new
+#     end
+#   end
+
+#   private
+
+#   def food_params
+#     params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
+#   end
+# end
+
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to @food
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
+end

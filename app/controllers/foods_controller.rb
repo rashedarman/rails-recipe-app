1761a3118f581a86340c8f2b6a21d1c399093ae0
Booze33 <tebetisloh@gmail.com>
2023-08-02 04:11:03 -0500

    Made changes

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,39 +1,7 @@
-# class FoodsController < ApplicationController
-#   def index
-#     @user = current_user
-#     @foods = @user&.foods || []
-#   end
-
-#   def show
-#     @food = Food.find(params[:id])
-#   end
-
-#   def new
-#     @food = Food.new
-#   end
-
-#   def create
-#     @food = Food.new(food_params.merge(user: current_user))
-  
-#     if @food.save
-#       flash[:success] = 'Food added successfully'
-#       redirect_to root_path
-#     else
-#       flash.now[:error] = 'Failed to add food'
-#       render :new
-#     end
-#   end
-
-#   private
-
-#   def food_params
-#     params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
-#   end
-# end
-
 class FoodsController < ApplicationController
   def index
-    @foods = Food.all
+    @user = current_user
+    @foods = @user&.foods || []
   end
 
   def show
@@ -45,10 +13,13 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = Food.new(food_params)
+    @food = Food.new(food_params.merge(user: current_user))
+  
     if @food.save
-      redirect_to @food
+      flash[:success] = 'Food added successfully'
+      redirect_to root_path
     else
+      flash.now[:error] = 'Failed to add food'
       render :new
     end
   end
@@ -56,6 +27,6 @@ class FoodsController < ApplicationController
   private
 
   def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+    params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
   end
 end

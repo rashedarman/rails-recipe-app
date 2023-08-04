870caaa2cacfe3fa43318cc78c43ba35fb38fb08
Booze33 <tebetisloh@gmail.com>
2023-08-04 10:53:59 -0500

    Adjusting the nav_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def devise_controller?
+    devise_controller_names.include?(controller_name)
+  end
+
+   private
+
+   def devise_controller_names
+    %w[registrations sessions passwords confirmations]
+  end
 end

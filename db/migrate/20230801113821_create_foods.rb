1d75eba93288bed7d5acdf0fd42d146366b3d8f7
Rashed Arman <rashedarman21@gmail.com>
2023-08-05 18:56:19 +0600

    make user confirmable

--- db/migrate/20230801113821_create_foods.rb
+++ db/migrate/20230801113821_create_foods.rb
@@ -4,7 +4,7 @@ class CreateFoods < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :measurement_unit
       t.float :price
-      t.float :quantity
+      t.integer :quantity
       t.references :user, null: false, foreign_key: true, index: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_02_112455) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_195616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_112455) do
     t.string "name"
     t.string "measurement_unit"
     t.float "price"
-    t.float "quantity"
+    t.integer "quantity"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -56,6 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_112455) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
